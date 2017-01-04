8db3a6e3e46822dbcec7e9f3c721325cf714d599
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-04 10:43:22 +0100

    routes on Slim framework

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ $app->get("/hola/:name", function($name){
   echo "Hola ". $name;
 });
 
-$app->get("/pruebas/:uno/:dos", function($uno, $dos){
+$app->get("/pruebas(/:uno/:dos)", function($uno=NULL, $dos=NULL){
   echo $uno."<br>";
   echo $dos."<br>";
-});
+})->conditions(array(
+  "uno" => "[a-zA-Z]*",
+  "dos" => "[0-9]*"
+));
+
 $app->run();
 ?>
