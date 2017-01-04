57250168be8d94c6aee7473cd0f52c4f9c3dfd71
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-04 10:59:54 +0100

    using middleware

--- index.php
+++ index.php
@@ -5,7 +5,17 @@ $app->get("/hola/:name", function($name){
   echo "Hola ". $name;
 });
 
-$app->get("/pruebas(/:uno/:dos)", function($uno=NULL, $dos=NULL){
+function pruebaMiddle(){
+  echo "I'm a middleware";
+
+}
+
+function pruebaTwo (){
+  echo "I'm a middleware";
+
+}
+
+$app->get("/pruebas(/:uno/:dos)", 'pruebaMiddle', 'pruebaTwo', function($uno=NULL, $dos=NULL){
   echo $uno."<br>";
   echo $dos."<br>";
 })->conditions(array(
